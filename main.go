d89ad910ef5adc3402520be88433dd4fb5c90b4f
BipRaider <thebipus@gmail.com>
2019-01-07 16:11:19 +0200

    Added a conteant-type header

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func habdlerFunc(w http.ResponseWriter, r *http.Request) { //Данную функцию можно назвать как угодно
-	// print("Сhac brauzer ") // log_color_app:     bold_white    при дабовление это в runner.conf   пичает в терменале какждый раз как обновит в браузере страницу
+	print("Сhac brauzer ") // log_color_app:     bold_white    при дабовление это в runner.conf   пичает в терменале какждый раз как обновит в браузере страницу
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcom to my as2 site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", habdlerFunc) // спомощью этой функций отправляется выше созданая функция адрес
 	//"/"  открываться будет отсюда  и все следующие страницы http://localhost:3000
-
 	//"/dog/" Строгая ,Страница будет открываться после http://localhost:3000/dog/
 
 	http.ListenAndServe(":3000", nil) // это адрес сервера  куда будет отправляться данные
