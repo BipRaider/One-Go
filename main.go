bcc37c3946fdf7de8ef31a5efa675cafd5431aa0
BipRaider <thebipus@gmail.com>
2019-01-07 15:43:27 +0200

    Adde a fresh config file

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func habdlerFunc(w http.ResponseWriter, r *http.Request) { //Данную функцию можно назвать как угодно
-	fmt.Fprint(w, "<h1>Welcom to my awesone site!</h1>")
+	// print("Сhac brauzer ") // log_color_app:     bold_white    при дабовление это в runner.conf   пичает в терменале какждый раз как обновит в браузере страницу
+	fmt.Fprint(w, "<h1>Welcom to my as2 site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", habdlerFunc) // спомощью этой функций отправляется выше созданая функция адрес
+	//"/"  открываться будет отсюда  и все следующие страницы http://localhost:3000
+
+	//"/dog/" Строгая ,Страница будет открываться после http://localhost:3000/dog/
+
 	http.ListenAndServe(":3000", nil) // это адрес сервера  куда будет отправляться данные
 }
 
