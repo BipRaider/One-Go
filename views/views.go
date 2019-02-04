123c4b7f3560d1654f11e8f88bb659aedfc3b536
BipRaider <thebipus@gmail.com>
2019-02-04 20:48:08 +0200

    Made it easier to create views by not passing in the views/ prefix and by not requirung the gohtml extension

--- views/views.go
+++ views/views.go
@@ -1,7 +1,6 @@
 package views
 
 import (
-	"errors"
 	"html/template"
 	"net/http"
 	"os"
@@ -15,13 +14,12 @@ type View struct {
 
 // функция для обьявления какой шаблон использовать и хранящийся  файл (чтобы потом выводить в браузере)   1.1
 func NewView(layout string, files ...string) *View {
-	//Добовляем данные из файла
+	addTemplatePath(files)
+	addTemlateExt(files)
 	files = append(files, layoutFiles()...)
-
 	t, err := template.ParseFiles(files...) // Записываем в переменую t значаения(срез  файлов ) с  файла по сылке
 
 	if err != nil {
-		err = errors.New("ERROR func views/NewView at ParseFiles")
 		os.Exit(5)
 	}
 	return &View{
@@ -32,17 +30,45 @@ func NewView(layout string, files ...string) *View {
 
 var (
 	LayoutDir   string = "views/layouts/"
+	TemplateDir string = "views/"
 	TemplateExt string = ".gohtml"
 )
 
-// this func переберает  все files ,what есть в папке LayoutDir
+// this func переберает  все files ,what есть в папке LayoutDir  1.1.2
 // (*)- говорит что все файлы должны соотвецтвовать макету (типу файлу ,что указано )
-func layoutFiles() []string {
+func layoutFiles() []string { //Добовляем данные из файла
+
 	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
 	if err != nil {
 		os.Exit(7)
 	}
 	return files
+}
+
+//1.1.3
+// addTemplatePath  takes in a slice of strings
+// representing file paths for templates and it prepends // представляет файл пути за шаблоном и это добовляет
+//the TamplateDir directory to each string in the slice  //каталог TamplateDir для каждой строки в срезе
+//
+//Eg the input {"home"}would result in the output
+//{"views/home"} if TemplateDir == "views/"
+func addTemplatePath(files []string) { //Добавит "views/"
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+//1.1.4
+//addTemplateExt  takes in a slice of strings
+//representing file paths for templates and it prepends
+//the TamplateExt extension to each string in the slice
+//
+//Eg the input {"home"}would result in the output
+//{"home.gohmtl"} if TemplateExt ==".gohtml"
+func addTemlateExt(files []string) { //Добавит ".gohml"
+	for i, f := range files {
+		files[i] = f + TemplateExt
+	}
 }
 
 // Функция  которая выводит в браузер нужную файл и какого шаблона , 1.2
