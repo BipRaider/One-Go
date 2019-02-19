10166feecef8e00ff835bf1d7b8051ebf2eab102
BipRaider <thebipus@gmail.com>
2019-02-19 19:52:51 +0200

    Showe the relational data with GORM demo

--- exp/LesDB.go
+++ exp/LesDB.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql" // подсоединить библиотеку mysql
@@ -12,64 +10,49 @@ import (
 
 type UserGorm struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"` // добовления типа данных в базуданых
-	Color string
+	Name   string
+	Email  string `gorm:"not null;unique_index"` // добовления типа данных в базуданых
+	Color  string
+	Orders []Orders `gorm:"ForeignKey:UserId"` // `gorm:"ForeignKey:RecordId"` надо указать для обьядинения библиотек
+	// чтобы можно было их присоединять  через  db.Preload() http://doc.gorm.io/crud.html#preloading-eager-loading
+}
+type Orders struct {
+	gorm.Model
+	UserID      uint
+	Amount      int    //количество
+	Description string // описание
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "root:alfadog1@/bipusdb?parseTime=true") //Соединение с базой данных  !!ВАЖНО ?parseTime=true  добисывать в конце если надо чтобы выводило время
+
+	db, err := gorm.Open("mysql", "root:alfadog1@/bipusdb?charset=utf8&parseTime=True&loc=Local") //Соединение с базой данных  !!ВАЖНО ?parseTime=true  добисывать в конце если надо чтобы выводило время
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	//db.LogMode(true)            // отображает лог строки запроса в терменале
-	db.AutoMigrate(&UserGorm{}) // автомотически отправляет данные в базу даных
+	db.LogMode(true)                       // отображает лог строки запроса в терменале
+	db.AutoMigrate(&UserGorm{}, &Orders{}) // автомотически отправляет данные в базу даных
 
 	var u UserGorm
-	name, email, color := getInfp() // выводит данные для дальнейшей записи
-	us := UserGorm{
-		Name:  name,
-		Email: email,
-		Color: color,
-	}
-	if err = db.Create(&us).Error; err != nil { // ЗАписывет данные в базу-данных
-		os.Exit(1)
-	} else {
-		db.Last(&us)
-		fmt.Println("Last name created in datanase \n", us)
-	}
-	// newDB := db.Where("email=?", "blah@blah.com")
-	// newDB = newDB.Or("color=?", "wite")             // функция или
-	// newDB = newDB.First(&u)
-	// db = db.Where("email = ?", "bipus@gmail.com").First(&u)
-	if err := db.Where("email = ?", "bipus@gmail.com").First(&u).Error; err != nil {
-		switch err {
-		case gorm.ErrRecordNotFound:
-			fmt.Println("1111100011111")
-		case gorm.ErrInvalidSQL:
-			fmt.Println("sql")
-		default:
-			os.Exit(33)
-		}
-	}
 
+	if err := db.Preload("Orders").Find(&u).Error; err != nil {
+		os.Exit(3)
+	}
 	fmt.Println(u)
 
-}
+	// createOrder(db, u, 1001, "Fake description #4")
+	// createOrder(db, u, 231, "Fake description #5")
+	// createOrder(db, u, 101, "Fake description #6")
 
-// запрос нужных данных для записи в базу данных
-func getInfp() (name, email, color string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What id your name?")
-	name, _ = reader.ReadString('\n') // Функция для запроса в вода fmt.Scan
-	fmt.Println("Whatewr idewr yourer nameqweqwe?")
-	email, _ = reader.ReadString('\n')
-	fmt.Println("Whatewr color?")
-	color, _ = reader.ReadString('\n')
-	name = strings.TrimSpace(color) // Возвращает срез строки  без ( \n )
-	name = strings.TrimSpace(name)
-	email = strings.TrimSpace(email)
-	return name, email, color
+}
+func createOrder(db *gorm.DB, user UserGorm, amount int, dest string) {
+	err := db.Create(&Orders{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: dest,
+	}).Error
+	if err != nil {
+		os.Exit(3)
+	}
 }
