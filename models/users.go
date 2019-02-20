23737c06a52a28151c2460a01a41fb03d53e78a3
BipRaider <thebipus@gmail.com>
2019-02-20 17:41:07 +0200

    Creade the UserService for interacting with user models

--- models/users.go
+++ models/users.go
@@ -1,9 +1,64 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var (
+	//ErrNorFound is returned when a resource cannot be found
+	// in the database.
+	ErrNorFaund = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	//Соединение с базой данных  !!ВАЖНО ?parseTime=true  добисывать в конце если надо чтобы выводило время
+	db, err := gorm.Open("mysql", connectionInfo) //"root:alfadog1@/bipusdb?charset=utf8&parseTime=True&loc=Local"
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+//ByID will look up by the id provided
+//1 - user ,nil
+//2 - nil, ErrorNotFound
+//3 - nil, otherError
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ? ", id).
+		First(&user).
+		Error
+
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNorFaund
+	default:
+		return nil, err
+	}
+}
+
+//Фнкция для closes the Service with database
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+//DestructiveReset drops the user table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
 
 type User struct {
 	gorm.Model
