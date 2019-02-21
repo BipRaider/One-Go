46ef2334b0be857c5884e31f499afeac50cf0da1
BipRaider <thebipus@gmail.com>
2019-02-21 17:09:46 +0200

    New commit

--- exp/LesDB.go
+++ exp/LesDB.go
@@ -33,13 +33,21 @@ func main() {
 		panic(err)
 	}
 	defer us.Close()
-	//us.DestructiveReset()
-	user, err := us.ByID(3)
+	// us.DestructiveReset()
+	// user := models.User{
+	// 	Name:  "Micheal Scott",
+	// 	Email: "Micheal@gmail.com",
+	// }
+	// if err := us.Create(&user); err != nil {
+	// 	os.Exit(2)
+	// }
+	user, err := us.ByID(1)
 	if err != nil {
 		os.Exit(1)
 	}
 
 	fmt.Println(user)
+
 	// db, err := gorm.Open("mysql", mysqlinfo) //Соединение с базой данных  !!ВАЖНО ?parseTime=true  добисывать в конце если надо чтобы выводило время
 	// if err != nil {
 	// 	panic(err)
