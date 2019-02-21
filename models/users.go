08291a955d2a24043c72c6261f67a27337569e34
BipRaider <thebipus@gmail.com>
2019-02-21 18:35:27 +0200

    Created package user_test ant tested

--- models/users.go
+++ models/users.go
@@ -16,6 +16,7 @@ var (
 func NewUserService(connectionInfo string) (*UserService, error) {
 	//Соединение с базой данных  !!ВАЖНО ?parseTime=true  добисывать в конце если надо чтобы выводило время
 	db, err := gorm.Open("mysql", connectionInfo) //"root:alfadog1@/bipusdb?charset=utf8&parseTime=True&loc=Local"
+
 	if err != nil {
 		return nil, err
 	}
@@ -54,6 +55,13 @@ func (us *UserService) ByID(id uint) (*User, error) {
 func (us *UserService) Create(user *User) error {
 
 	return us.db.Create(user).Error
+}
+
+//Update will update the provided user with all of the database
+//in the  provaided user object
+func (us *UserService) Update(user *User) error {
+	return us.db.Update(user).Error
+
 }
 
 //Фнкция для closes the Service with database
